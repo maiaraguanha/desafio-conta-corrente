969d345e24c2b59d9ab715ff86c3b1bfebdfd877
Raphael <50387178+maiaraguanha@users.noreply.github.com>
2023-08-25 17:15:16 -0300

    alteração no menu

---
+++ src/Main.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String nome = "Maiara Franciely";
+        String tipoConta = "Corrente";
+        double saldo = 2500.00;
+
+        System.out.println("********************************************************");
+        System.out.println("\nDados iniciais do cliente:");
+        System.out.println("\nNome:               " + nome);
+        System.out.println("Tipo de conta:      " + tipoConta);
+        System.out.println("Saldo:              R$  " + saldo);
+        System.out.println("\n********************************************************");
+
+
+        String operacoesDoCliente = """
+                                    
+                Operações
+                
+                1- Consultar saldo
+                2- Receber valor
+                3- Transferir valor
+                4- Sair
+                
+                Digite a opção desejada:
+                
+                """;
+
+        Scanner lerValor = new Scanner(System.in);
+        int opcao = 0;
+
+
+        while (opcao != 4) {
+
+            System.out.println(operacoesDoCliente);
+            opcao = lerValor.nextInt();
+
+            if (opcao == 1) {
+                System.out.println("O saldo atual é R$ " + saldo);
+            }else if (opcao == 2) {
+                double valorRecebido = 0;
+                Scanner recebido = new Scanner(System.in);
+                System.out.println("Informe o valor a receber: ");
+                valorRecebido = recebido.nextDouble();
+                saldo += valorRecebido;    // saldoAtual = saldo + valorRecebido;
+                System.out.println("Saldo atualizado R$ " + saldo);
+            } else if (opcao == 3) {
+                double valorSacado = 0;
+                Scanner sacado = new Scanner(System.in);
+                System.out.println("Informe o valor que deseja transferir: ");
+                valorSacado = sacado.nextDouble();
+
+                if (valorSacado <= saldo) {
+                    saldo-= valorSacado;  // saldoAtual = saldoAtual - valorSacado;
+                    System.out.println("Saldo atualizado R$ " + saldo);
+                } else {
+                    System.out.println("Saldo insuficiente para essa transferência");
+                }
+            } else if (opcao == 4) {
+                System.out.println("Obrigada por escolher o nosso Banco! Volte Sempre !!!");
+            } else {
+                System.out.println("Operação inválida.");
+            }
+        }
+    }
+}
